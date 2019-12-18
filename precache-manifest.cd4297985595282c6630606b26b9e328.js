6c8acc5fefe72eca7264ebc21337e0080e1c6631
r-smolonskij <ruslans.smolonskis@gmail.com>
2019-12-18 14:11:30 +0200

    Updates

---
+++ precache-manifest.cd4297985595282c6630606b26b9e328.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0565597e1cf197fe56e383f8994a7e76",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f4059c059e8d9d2b63d2",
+    "url": "/monsters-rolodex/static/css/main.e74cfbb6.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "f4059c059e8d9d2b63d2",
+    "url": "/monsters-rolodex/static/js/main.eb3d1599.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
